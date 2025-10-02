519720be3c8f9fdf3391f9371b58784067659690
Jose Emmanuel M. Mendoza <143564063+man-gg@users.noreply.github.com>
2025-10-02 17:40:34 +0800

    Delete test_gemini.py

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyAtqJe99Ch84hfYfNCDDq6fBoTuG8siYao")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Are you Gemini AI? Yes or no?"
-)
-print(response.text)
